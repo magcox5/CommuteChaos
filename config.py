940529950960dc094ba1144ca13a3f376f6ad130
Molly_888 <magcox5@yahoo.com>
2020-01-18 20:25:17 -0800

    Corrected bar graphs and added map to Charts notebook

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-gkey = "INSERT YOUR KEY HERE"
+# Enter your API key
+#gkey = "AIzaSyDa-OAyuw7eydA74od3DgKh5kTNCiXjI3o"
+gkey = "AIzaSyDI-I5IDvq-3uTimrZ8HbqlXsANF8Y0zF0"
---
+++ config_old.py
@@ -0,0 +1,1 @@
+gkey = "INSERT YOUR KEY HERE"
